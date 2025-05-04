a12efda7787445a73edf34e5009b374234f52f4a
Your Name <your.email@example.com>
2025-05-04 21:42:35 +0530

    Add homepage to fix 404 error

--- Desktop/jsm_mock_interview_platform/app/page.tsx
+++ Desktop/jsm_mock_interview_platform/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
     return (
         <main className="flex items-center justify-center h-screen">
-            <h1 className="text-3xl font-bold">Welcome to JSM Mock Interview Platform 🚀</h1>
+            <h1 className="text-3xl font-bold">Welcome to VocaBot 🚀</h1>
         </main>
     );
 }
