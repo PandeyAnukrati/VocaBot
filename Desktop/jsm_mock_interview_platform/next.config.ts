083a59973dd4d5e2053621985efdb57cb83af5f2
PandeyAnukrati <162144090+PandeyAnukrati@users.noreply.github.com>
2025-05-05 17:02:45 +0530

    Update next.config.ts

--- Desktop/jsm_mock_interview_platform/next.config.ts
+++ Desktop/jsm_mock_interview_platform/next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
  eslint:{
-  ignoreDuringBuilds:true,
+  ignoreDuringBuilds:false,
 
  },
  typescript:{
-  ignoreBuildErrors:true,
+  ignoreBuildErrors:false,
  }
 
 };
