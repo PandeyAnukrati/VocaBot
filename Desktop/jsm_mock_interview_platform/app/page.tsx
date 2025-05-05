d688ba923a9ce669e2c018cb9a2acee8bf16d90f
PandeyAnukrati <162144090+PandeyAnukrati@users.noreply.github.com>
2025-05-05 18:14:47 +0530

    Update page.tsx

--- Desktop/jsm_mock_interview_platform/app/page.tsx
+++ Desktop/jsm_mock_interview_platform/app/page.tsx
@@ -1,8 +1,9 @@
 // app/page.tsx
 export default function Home() {
     return (
-        <main className="flex items-center justify-center h-screen">
-            <h1 className="text-3xl font-bold">Welcome to VocaBot 🚀</h1>
-        </main>
+        <div>
+            <h1>Welcome to VocaBot</h1>
+            <p>This is the homepage.</p>
+        </div>
     );
 }
